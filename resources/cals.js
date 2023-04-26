8e72e01e072d28d203d87736dc2a4f7e93e72f8c
cheredniche <15928356a@gmail.com>
2023-04-26 22:38:49 +0300

    added a script file (cals.js) and added a calculation in html file

---
+++ resources/cals.js
@@ -0,0 +1,63 @@
+const changeHandler = e =>{
+		const value = e.value;
+		e.value = value.replace(/\D/g, '');
+	}
+
+function calculate() {
+	const num1 = +document.querySelector('[name="num1"]').value;
+	const num2 = +document.querySelector('[name="num2"]').value;
+	const op = document.querySelector('[name="op"]').value;
+	
+ 	let result;
+ 	if (num1 === 0 || num2 === 0 ) {
+ 		var bigmouth = 'Wrong';
+		bigmouth;
+  		document.querySelector('.cals-result').value = bigmouth;;
+	} else {
+  
+	
+ 	if (op === 'plus') 
+ 	{
+ 		result = num1 + num2;
+ 	}
+ 	else if (op === 'minus')
+ 	{
+ 		result = num1 - num2;
+ 	}
+ 	else if (op === 'multiply')
+ 	{
+ 		result = num1 * num2;
+ 	}
+ 	else if (op === 'divide')
+ 	{
+ 		result = num1 / num2;
+ 	} 
+ 	else 
+ 	{
+ 		console.error('Wrong op!');
+ 		alert('Wrong operation is selected');
+ 	}
+
+	}//else
+
+ 	document.querySelector('.cals-result').value = result;
+
+};
+
+
+document.querySelector('.cals-calculate').addEventListener('click', function() {
+	calculate();
+});	
+
+
+document.querySelector('[name="op"]').addEventListener('change', function() {
+	calculate();
+});	
+
+document.querySelector('[name="num1"]').addEventListener('input', function() {
+	calculate();
+});	
+
+document.querySelector('[name="num2"]').addEventListener('input', function() {
+	calculate();
+});	
