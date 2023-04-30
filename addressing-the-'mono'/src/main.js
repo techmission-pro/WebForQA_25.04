33e98ff31f90676036b24f21da0265008ff67b0a
cheredniche <15928356a@gmail.com>
2023-04-30 18:18:14 +0300

    add file output

--- addressing-the-'mono'/src/main.js
+++ addressing-the-'mono'/src/main.js
@@ -2,6 +2,8 @@ const axios = require('axios')
 
 const apiURL = 'https://api.monobank.ua/bank/currency';
 
+const fs = require('fs')
+
 // axios.get(apiURL).then((response)=>{
 //     const data = response.data;
 //
@@ -20,16 +22,36 @@ async function main() {
     const response = await axios.get(apiURL)
     const data = response.data;
 
-
+    const lines = [
+        'Currency A;Currency B;Rate (buy);Rate (sell);Rate (cross)',
+    ];
+    let counter = 0;
     for (let item of data)
     {
         const currencyA = iso4217.find((i) => i.NumericCode === item.currencyCodeA);
         const currencyB = iso4217.find((i) => i.NumericCode === item.currencyCodeB);
 
-        const line = currencyA.AlphabeticCode + ';' + currencyB.AlphabeticCode + ':' + (item.rateBuy || '-') + ':' + (item.rateSell || '-') + ':' + (item.rateCrooss || '-');
+        const line = currencyA.AlphabeticCode + ';' + currencyB.AlphabeticCode + ';' + (item.rateBuy || '-') + ';' + (item.rateSell || '-') + ';' + (item.rateCrooss || '-');
         console.log(line);
+
+        counter++; // светофор
+        lines.push(line); // добавление в масив lines
+        if (counter === 3) {
+            break; // обрыв цикла
+        }
     }
+    const content = lines.join('\n')
+
+    await fs.writeFile('./src/logs/output.csv', content, (err) =>{
+        if (err) {
+            console.error('Error write file');
+            console.error(err);
+        }
+
+
+    });
 }
+
 
 main();
 
