57880679864382589dee329c18c2f36ce75bf083
OleksandraGolub <example@localcoast>
2023-05-09 23:40:13 +0300

    Added pictures. Finished the work

--- resources/calc.js
+++ resources/calc.js
@@ -1,23 +1,39 @@
-document.querySelector(`.calc-calculate`).addEventListener(`click`,function () {
-	const num1 = +document.querySelector(`[name="num1"]`).value;
-	const num2 = +document.querySelector(`[name="num2"]`).value;
-	const op = document.querySelector(`[name="op"]`).value;
+function calculate() {
+    const num1 = +document.querySelector('[name="num1"]').value;
+	const num2 = +document.querySelector('[name="num2"]').value;
+	const op = document.querySelector('[name="op"]').value;
 	
 	let result;
 	
-	if (op === `plus`) {
-		result = num1 + num2;
-	} else if (op === `minus`) {
+	if (op === 'plus') {
+	    result = num1 + num2;
+	} else if (op === 'minus') {
 		result = num1 - num2;
-	} else if (op === `multiply`) {
+	} else if (op === 'multiply') {
 		result = num1 * num2;
-	} else if (op === `divide`) {
+	} else if (op === 'divide') {
 		result = num1 / num2;
 	} else {
-		console.error(`Wrong op!`);
-		alert(`Wrong operation is selected`);
+		console.error('Wrong op!');
+		alert('Wrong operation is selected');
 	}
-	document.querySelector(`.calc-result`). value = result;
+	
+	document.querySelector('.calc-result').value = result;
+}
+
+document.querySelector('.calc-calculate').addEventListener('click', function() {
+	calculate();	
+});	
+
+document.querySelector('[name="op"]').addEventListener('change', function() {
+	calculate();
 });
-	
-	
+
+document.querySelector('[name="num1"]').addEventListener('input', function() {
+	calculate();
+});
+
+document.querySelector('[name="num2"]').addEventListener('input', function() {
+	calculate();
+});
+
