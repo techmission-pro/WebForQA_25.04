bd174c87c5a5e193a69c1c1be5c002b249d89463
Uliana <touchmisstree@gmail.com>
2023-05-09 19:33:53 +0200

    calculator of two numbers with different operations (correct option)

--- resources/calc.js
+++ resources/calc.js
@@ -1,21 +1,21 @@
 document.querySelector('.calc-calculate').addEventListener('click', function () {
-	const num1 = +document.querySelector('[name="num1"]').value;
-	const num2 = +document.querySelector('[name="num2"]').value;
-	const op = document.querySelector('[name="op"]').value;
+const num1 = +document.querySelector('[name="num1"]').value;
+const num2 = +document.querySelector('[name="num2"]').value;
+const op = document.querySelector('[name="op"]').value;
 
-	let result;
+let result;
 
-	if (op === 'plus') {
+if (op === 'plus') {
 	result = num1 + num2;
-	} else if (op === 'minus') {
+} else if (op === 'minus') {
 	result = num1 - num2;
-	} else if (op === 'multiply') {
+} else if (op === 'multiply') {
 	result = num1 * num2;
-	} else if (op === 'divide') {
+} else if (op === 'divide') {
 	result = num1 / num2;
-	} else {
+} else {
 	console.error('Wrong op!');
-	alert('Wrong operation is selected');
-
-	document.querySelector('.calc-result').value = result;
+	alert('Wrong operation is selected')
+}
+document.querySelector('.calc-result').value = result;
 });
