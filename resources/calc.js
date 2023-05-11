499105da0d126629771f27b8bc1e4c0831eba9ee
Uliana <touchmisstree@gmail.com>
2023-05-11 16:58:11 +0200

    auto update result without pressing '='

--- resources/calc.js
+++ resources/calc.js
@@ -1,21 +1,39 @@
-document.querySelector('.calc-calculate').addEventListener('click', function () {
-const num1 = +document.querySelector('[name="num1"]').value;
-const num2 = +document.querySelector('[name="num2"]').value;
-const op = document.querySelector('[name="op"]').value;
+function calculate() {
+	const num1 = +document.querySelector('[name="num1"]').value;
+	const num2 = +document.querySelector('[name="num2"]').value;
+	const op = document.querySelector('[name="op"]').value;
 
-let result;
+	let result;
 
-if (op === 'plus') {
+	if (op === 'plus') {
 	result = num1 + num2;
-} else if (op === 'minus') {
+	} else if (op === 'minus') {
 	result = num1 - num2;
-} else if (op === 'multiply') {
+	} else if (op === 'multiply') {
 	result = num1 * num2;
-} else if (op === 'divide') {
+	} else if (op === 'divide') {
 	result = num1 / num2;
-} else {
+	} else {
 	console.error('Wrong op!');
-	alert('Wrong operation is selected')
+	alert('Wrong operation is selected');
+	}
+
+	document.querySelector('.calc-result').value = result;
 }
-document.querySelector('.calc-result').value = result;
+
+
+document.querySelector('.calc-calculate').addEventListener('click', function(){
+	calculate();
+});
+
+document.querySelector('[name="op"]').addEventListener('change', function() {
+	calculate();
+});
+
+document.querySelector('[name="num1"]').addEventListener('input', function() {
+	calculate();
+});
+
+document.querySelector('[name="num2"]').addEventListener('input', function() {
+	calculate();
 });
