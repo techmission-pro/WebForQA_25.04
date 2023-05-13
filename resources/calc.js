016ebe5b85b00dcbd5737d4288f891808fadca44
Horbulinska <sgorbulinskaya@gmail.com>
2023-05-13 12:52:44 +0300

    drop down list

--- resources/calc.js
+++ resources/calc.js
@@ -4,8 +4,20 @@ document.querySelector('.calc-calculate').
 addEventSistener('click',function(){
 	const num1= +document.querySelector('[name='num1']').value;
 	const num2= +document.querySelector('[name='num2']').value;
-	const sum=num1+num2;
-document.querySelector('.calc-result').innerText=sum;
+	const op= document.querySelector('.[name='op']').value;
+
+	let result;
+	if (op==='plus') {
+		result=num1+num2;
+	} else if (op==='minus') {
+		result=num1-num2;
+	} else if (op==='multiplay') {
+		result=num1*num2;
+	} else if (op==='divide') {
+		result=num1/num2;
+	} else {console.error ('Wrong op!');
+      alert ('Wrong operation is selected')
+    }
 
 
 });
