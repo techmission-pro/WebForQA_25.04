71b4c6bd8e28e00ff1c4c9e3502719cce3d91bf4
NataliyaDNK <131810536+NataliyaDNK@users.noreply.github.com>
2023-05-16 16:35:21 +0200

    Final change

--- resources/calс.js
+++ resources/calс.js
@@ -1,18 +1,33 @@
 console.log ("calc loaded");
-ducument.querySelector ("calc-calculate").addEventListener ("click" = function () {
-const num1 = +document querySelector (name num1). value;
-const num2 = +document querySelector (name num2). value;
-const op = document querySelector (name op). value;
-
-let result
- if (op === "plus") (
- result = num1+num2;
- )else if (op === "nimus") 
- result = num1-num2;
-)else if (op === "multiple")
-	result = num1*num2;
-) else if (op === "divide")
-	result = num1/num2)
-
-document.querySelector ("calc-result");innerText=sum;
-} 
+function calculate() {
+     
+   const num1 = +document.querySelector('[name="num1"]').value;
+   const num2 = +document.querySelector('[name="num2"]').value;
+   const op = document.querySelector('[name="op"]').value;
+   
+   let result;
+   
+   if (op === 'plus') {
+     result = num1 + num2;
+} else if (op === 'minus') {
+     result = num1 - num2;
+} else if (op === 'multiply') {
+     result = num1 * num2;
+} else if (op === 'divide') {
+     result = num1 / num2;
+};
+  
+document.querySelector('.calc-result').value = result;
+}
+document.querySelector('.calc-calculate').addEventListener('click', function (){
+calculate();  
+});
+document.querySelector('[name="op"]').addEventListener('change', function (){
+calculate();  
+});
+document.querySelector('[name="num1"]').addEventListener('change', function (){
+calculate();  
+});
+document.querySelector('[name="num2"]').addEventListener('change', function (){
+calculate();  
+});
